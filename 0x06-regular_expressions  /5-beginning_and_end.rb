f27428244b06b3d657c1e02ee2d60357a8804548
Omarkh2021 <90703502+Omarkh2021@users.noreply.github.com>
2024-01-05 21:54:08 +0200

    Update 5-beginning_and_end.rb

--- 0x06-regular_expressions  /5-beginning_and_end.rb
+++ 0x06-regular_expressions  /5-beginning_and_end.rb
@@ -1,2 +1,13 @@
-#!/usr/bin/env ruby
-puts ARGV[0].scan(/^h.n$/).join
+```ruby
+#!/usr/bin/ruby
+
+def beginning_and_end(string)
+  if string[0..1] == "hn" && string[-1..-1] == "n"
+    puts "$"
+  else
+    puts "h#{'x' if string[-1] == 'n'}#{'x' if string[0] == 'h'}tn$"
+  end
+end
+
+beginning_and_end(ARGV[0])
+```
