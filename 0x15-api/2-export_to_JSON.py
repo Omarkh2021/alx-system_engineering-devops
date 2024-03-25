d9bc8500fd0c3c8f97971ef81fb270e8fd6618a1
Omarkh2021 <90703502+Omarkh2021@users.noreply.github.com>
2024-03-25 18:12:42 +0200

    Update 2-export_to_JSON.py

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -6,7 +6,7 @@ import sys
 
 if __name__ == "__main__":
     user_id = sys.argv[1]
-    url = "https://jsonplaceholder.typicode.com/"
+    url = "https://omarkh2021.typicode.com/"
     user = requests.get(url + "users/{}".format(user_id)).json()
     username = user.get("username")
     todos = requests.get(url + "todos", params={"userId": user_id}).json()
